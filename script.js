94412a5447fc9dbbe8768598a3b1c4e0654ba701
ozibozkurt <ozipaki@gmail.com>
2022-05-27 11:06:47 +0100

    I changed my images.html

--- script.js
+++ script.js
@@ -1,37 +1,19 @@
-function getNumberOrString(value) {
-  // Convert a string value to a number if possible
-  let number_value = Number(value);
-  if (Number.isNaN(number_value)) {
-    return value
-  } else {
-    return number_value
-  }
-}
+var view;
 
 
+view = ['https://i.ibb.co/bB7FDBN/9.jpg', 'https://i.ibb.co/pjr7xK1/1.jpg', 'https://i.ibb.co/60rRxw4/2.jpg', 'https://i.ibb.co/w0S6JqJ/4.jpg', 'https://i.ibb.co/StxLvzy/5.jpg', 'https://i.ibb.co/nB78zgL/6.jpg', 'https://i.ibb.co/xDbxm0n/7.jpg', 'https://i.ibb.co/GxGbmn0/8.jpg', 'https://i.ibb.co/SRpZSBd/22.jpg', 'https://i.ibb.co/yf4n5Ks/10.jpg', 'https://i.ibb.co/7nZGwc7/11.jpg', 'https://i.ibb.co/tMxDLFn/12.jpg', 'https://i.ibb.co/sCbvcd7/13.jpg', 'https://i.ibb.co/F6G5q4T/15.jpg', 'https://i.ibb.co/0tFqwVX/16.jpg', 'https://i.ibb.co/0spVWnR/17.jpg', 'https://i.ibb.co/D5NdDg9/18.jpg', 'https://i.ibb.co/wzfvj6S/19.jpg', 'https://i.ibb.co/vhcvYcG/20.jpg', 'https://i.ibb.co/dmNMd4S/21.jpg', 'https://i.ibb.co/SRpZSBd/22.jpg', 'https://i.ibb.co/J2JR8WW/25.jpg', 'https://i.ibb.co/34qKnSt/26.jpg', 'https://i.ibb.co/NLvJkQH/27.jpg'];
 
-document.getElementById('buttonn').addEventListener('click', (event) => {
-  let element_list = document.getElementById('list');
-  let new_li = document.createElement('li');
-  new_li.innerText = getNumberOrString(document.getElementById('textt').value);
 
-  element_list.appendChild(new_li);
+document.getElementById('next').addEventListener('click', (event) => {
+  view.push(view.shift());
+  let element_myphotos2 = document.getElementById('myphotos2');
+  element_myphotos2.setAttribute("src", view[0]);
 
 });
 
-var fruits;
-
-
-fruits = ['Employers are looking for Full stack developers that are:   1. Full stack developer should be master of essential front-end technologies like HTML5, CSS3, JavaScript. Knowledge of third-party libraries like jQuery, LESS, Angular and ReactJS is desirable 2. Full stack engineer should know at least one server-side programming languages like Java, Python, Ruby, .Net etc. 3. A version control system allows full stack developers to keep track of all the changes made in the codebase. The knowledge of Git helps full stack developers to understand how to get the latest code, update parts of the code, make changes in other developer’s code without breaking things.', 'Employers are looking for Front end developers that are:  1. A front-end developer creates websites and applications using web languages such as HTML, CSS, and JavaScript allow users to access and interact with the site or app. When you visit a website, the design elements you see were created by a front-end developer. 2. HTML, CSS, and JavaScript: These three languages are essential to anyone who wants to work in front-end development. HTML, CSS, and JavaScript work together to determine the look and functionality of the page.  3. Creativity, Problem-solving, Communication, Teamwork', 'Employers are looking for QA testers that are:  1. A company’s product must meet its customers\' requirements. A skilled QA tester knows how to think from a customer’s perspective throughout the entire software development cycle; we might recast this as "empathy." For example, a good tester should be able to create many “What if” scenarios and make sure the software responds to each possibility. This way, when the customer encounters one of these scenarios, the software will still continue to work properly.  2. QA testers need to be comfortable with different testing techniques (such as automation versus manual testing), common tools, and at least a passing familiarity with the options. For example, manual-testing skills alone won’t cut it. If the job calls for automation testing, then that is what they should be able to do as well. But don\'t hire purely based on past experience; look for evidence that the job candidate has regularly learned new things. Someone who has become proficient in five older tools probably can learn the next five -- including some that aren\'t created yet.  3. Project changes are almost always part of the gig, which means that there is no single way to handle issues that develop. There is always room for self-improvement. QA testers must adapt quickly to new tools and techniques'];
-
-
-document.getElementById('button').addEventListener('click', (event) => {
-  if (!!fruits.length) {
-    let element_list = document.getElementById('list');
-    let new_li = document.createElement('li');
-    new_li.innerText = fruits.shift();
-
-    element_list.appendChild(new_li);
-  }
+document.getElementById('previous').addEventListener('click', (event) => {
+  view.unshift(view.pop());
+  let element_myphotos22 = document.getElementById('myphotos2');
+  element_myphotos22.setAttribute("src", view[0]);
 
 });
